8eab742bfe37d1401782abffa89b9cd78aa55ec9
SnowSurfer <drinkaziu@gmail.com>
2019-03-12 15:03:54 -0400

    create forecast util function

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const forecast = require('./utils/forecast');
 geocode('New York', (error, data) => {
   console.log(data);
 });
+
+forecast(40.7648, -73.9808, (error, forecastData) => {
+  console.log(forecastData);
+});
---
+++ utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (long, lat, callback) => {
+  const url = `https://api.darksky.net/forecast/e79b022885f7770c971c38a56eba6e08/${lat},${long}?units=us`;
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather services.', undefined); 
+    } else if (response.body.error) {
+      callback(response.body.error, undefined);
+    } else {
+      const forecast =  `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees outside. There is a ${response.body.currently.precipProbability}% chance of precipitation`;
+      callback(undefined, forecast);
+    }
+  });
+}
+
+module.exports = forecast; 
