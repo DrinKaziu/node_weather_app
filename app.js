b02264a60c3bd25638cfe651ca9fdc87cbab1862
SnowSurfer <drinkaziu@gmail.com>
2019-03-12 15:09:47 -0400

    chain callbacks to make dynamic

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('New York', (error, data) => {
-  console.log(data);
+geocode('Boston', (error, data) => {
+  if (error) {
+    console.log(error)
+  } else {
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`${data.location}:`);
+        console.log(forecastData);
+      }
+    });
+  }
 });
 
-forecast(40.7648, -73.9808, (error, forecastData) => {
-  console.log(forecastData);
-});
