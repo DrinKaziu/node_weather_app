a20810dc2d5322091a897a7877cd69bca714a4de
SnowSurfer <drinkaziu@gmail.com>
2019-03-13 13:20:22 -0400

    set up hbs and views/partials paths

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,27 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+//Define paths for Express config
+const publicPath = path.join(__dirname, ('../public'));
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Set up HandleBars and Views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Set up public directory to serve
+app.use(express.static(publicPath));
+
 app.get('/', (req, res) => {
-  res.send('This will be the home page');
+  res.render('index', {
+    name: 'Drin Kaziu',
+    title: 'Weather App'
+  });
 });
 
 app.get('/about', (req, res) => {
